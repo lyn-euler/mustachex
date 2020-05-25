ef8e18fe889701d908b1770af4d41a8821825018
Rodsevich <nicorodsevich@gmail.com>
2020-05-25 09:46:32 -0300

    test de guarda de has

--- test/mustachex_test.dart
+++ test/mustachex_test.dart
@@ -33,10 +33,10 @@ void main() {
     };
     var vars = VariablesResolver(classesJSON);
     test('in a nutshell example', () async {
-      var template = '{{greeting_pascalCase}} {{what_pc}}!';
+      var template = '{{greeting_pascalCase}} {{world_pc}}!';
       var vars = {'greeting': 'HELLO'};
       String fulfillmentFunction(MissingVariableException variable) {
-        if (variable.varName == 'what') {
+        if (variable.varName == 'world') {
           return 'WORLD';
         } else {
           return 'UNIVERSE';
@@ -46,6 +46,25 @@ void main() {
       var processor = MustachexProcessor(
           initialVariables: vars, missingVarFulfiller: fulfillmentFunction);
       expect(await processor.process(template), equals('Hello World!'));
+      expect(await processor.process('{{greeting_pc}} {{xxx_pc}}!'),
+          equals('Hello Universe!'));
+    });
+    test('guarda de has', () async {
+      var vars = {
+        'items': [
+          {'name': 'uno'},
+          {'name': 'dos'}
+        ]
+      };
+      var processor = MustachexProcessor(initialVariables: vars);
+      var template = '{{#hasItems}}{{#items}} -{{name}}{{/items}}{{/hasItems}}';
+      var procesado = await processor.process(template);
+      expect(procesado, contains('-uno'));
+      expect(procesado, contains('-dos'));
+      template = '{{#hasSorpos}}{{#items}} -{{name}}{{/items}}{{/hasSorpos}}';
+      procesado = await processor.process(template);
+      expect(procesado, isNot(contains('-uno')));
+      expect(procesado, isNot(contains('-dos')));
     });
   });
 }
